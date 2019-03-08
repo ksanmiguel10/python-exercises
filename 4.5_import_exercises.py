1c54f770678fedab952658da918c52a60b3223fe
Kathryn Salts <kathrynsalts@gmail.com>
2019-03-08 15:49:38 -0600

    final

--- 4.5_import_exercises.py
+++ 4.5_import_exercises.py
@@ -39,8 +39,8 @@ import json
 data = json.load(open('profiles.json'))
 
 # Total number of users
-for key in data:
-   tot_users = int((len(data)))
+# for key in data:
+#    tot_users = int((len(data)))
 # print(tot_users)
 
 # Number of active users
@@ -56,21 +56,56 @@ for key in data:
 # print(tot_users - total_active)
 
 # Grand total of balances for all users
-def bal_new(amt):
-    amt = amt[1:] 
-    amt = amt.replace(',', '') 
-    return float(amt)
+# def bal_new(amt):
+#     amt = amt[1:] 
+#     amt = amt.replace(',', '') 
+#     return float(amt)
 
-balances = [bal_new(user['balance']) for user in data]
+# balances = [bal_new(user['balance']) for user in data]
 # print(f'The total balance is $',sum(balances))
     
 # Average balance per user
-avg_bal = sum(balances) / len(balances)
-print(avg_bal)
-
+# avg_bal = sum(balances) / len(balances)
+# print('${:,.2f}'.format(avg_bal))
 
 # User with the lowest balance
+# lowest_balance = min(balances)
+# user = [user for user in data if bal_new(user['balance']) == lowest_balance][0]
+# print(user['name'])
+
 # User with the highest balance
+# highest_balance = max(balances)
+# user = [user for user in data if bal_new(user['balance']) == highest_balance][0]
+# print(user['name'])
+
 # Most common favorite fruit
-# Least most common favorite fruit
+# favorite_fruits = [user['favoriteFruit'] for user in data]
+
+# favorite_fruit_counts = {}
+# for fruit in favorite_fruits:
+#     if fruit in favorite_fruit_counts:
+#         favorite_fruit_counts[fruit] += 1
+#     else:
+#         favorite_fruit_counts[fruit] = 1
+
+# print(favorite_fruit_counts)
+
+# items = list(favorite_fruit_counts.items())
+# items.sort(key=lambda item: item[1])
+
+# print('The most common favorite fruit is: ',items[-1][0])
+
+# # Least most common favorite fruit
+# print('The least common favorite fruit is: ', items[0][0])
+
 # Total number of unread messages for all users
+# def unread_messages(greeting: str):
+#     start = 'You have '
+#     stop = ' unread messages.'
+#     start_index = greeting.index(start) + len(start)
+#     stop_index = greeting.index(stop)
+#     return int(greeting[start_index:stop_index])
+
+# greetings = [user['greeting'] for user in data]
+# unread_messages = [unread_messages(greeting) for greeting in greetings]
+# print('There are', sum(unread_messages), 'unread total messages.')
